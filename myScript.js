03e97bd2dfada406b67c1194a145c89c9af91d9c
nigranac <can.argin.16@gmail.com>
2020-09-26 00:51:00 +0300

    Fibonacci Series

--- myScript.js
+++ myScript.js
@@ -14,12 +14,12 @@ function fibonacci() {
   if (number < 1 || number === "") {
     document.getElementById("result").innerHTML = "⚡⚡Please Enter A Positive Number⚡⚡"
   }
-
+else{
   for (let i = 2; i < number + 1; i++) {
 
       fiboArray.push(fiboArray[i - 2] + fiboArray[i - 1]);
       
     }
   document.getElementById("result").innerHTML = `✨Fibonacci(${number}) ➞ ${fiboArray[number]}✨`;
-  
+}
 }
